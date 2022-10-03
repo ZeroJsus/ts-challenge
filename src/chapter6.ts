bbed29f59acd919c0d6e4edd33e5f07538c1ebb7
ZeroJsus <f723706588@qq.com>
2022-10-04 00:00:37 +0800

    update

--- src/chapter6.ts
+++ src/chapter6.ts
@@ -2,59 +2,59 @@
  * @Author: yangrongxin
  * @Date: 2022-09-25 11:24:14
  * @LastEditors: yangrongxin
- * @LastEditTime: 2022-09-25 11:55:38
+ * @LastEditTime: 2022-10-03 17:42:05
  */
-function deleteUser (user: { id?: number, name: string }): void {
-  delete user.id
-}
+// function deleteUser (user: { id?: number, name: string }): void {
+//   delete user.id
+// }
 
-interface LUser {
-  id?: number | string
-  name: string
-}
+// interface LUser {
+//   id?: number | string
+//   name: string
+// }
 
-const lUser: LUser = {
-  id: '32',
-  name: 'Xin Yang'
-}
+// const lUser: LUser = {
+//   id: '32',
+//   name: 'Xin Yang'
+// }
 
-deleteUser(lUser) // 型变中的协变 函数类型收缩
+// deleteUser(lUser) // 型变中的协变 函数类型收缩
 
-class Animal {}
-class Bird extends Animal {
-  chirp (): void {}
-}
-class Crow extends Bird {
-  caw (): void {}
-}
+// class Animal {}
+// class Bird extends Animal {
+//   chirp (): void {}
+// }
+// class Crow extends Bird {
+//   caw (): void {}
+// }
 
-function chirp (bird: Bird): Bird {
-  bird.chirp()
-  return bird
-}
+// function chirp (bird: Bird): Bird {
+//   bird.chirp()
+//   return bird
+// }
 
-chirp(new Animal())
-chirp(new Bird())
-chirp(new Crow()) // 函数返回值的协变
+// chirp(new Animal())
+// chirp(new Bird())
+// chirp(new Crow()) // 函数返回值的协变
 
-function clone (f: (b: Bird) => Bird): void {
-  // ...
-}
+// function clone (f: (b: Bird) => Bird): void {
+//   // ...
+// }
 
-function animalToBird (a: Animal): Bird {
-  // ...
-}
+// function animalToBird (a: Animal): Bird {
+//   // ...
+// }
 
-clone(animalToBird)
+// clone(animalToBird)
 
-function crowToBird (c: Crow): Bird {
-  // ...
-}
+// function crowToBird (c: Crow): Bird {
+//   // ...
+// }
 
-clone(crowToBird) // 函数对参数和this 做逆变
+// clone(crowToBird) // 函数对参数和this 做逆变
 
-const d = [1, { x: 2 }]
-const e = [1, { x: 2 }] as const
+// const d = [1, { x: 2 }]
+// const e = [1, { x: 2 }] as const
 
-console.log(d)
-console.log(e)
+// console.log(d)
+// console.log(e)
